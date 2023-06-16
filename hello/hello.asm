0f0a9dd9e796c7340648c5a5f228b7fb1cb1608f
Luiz Henrique <luizhen428@gmail.com>
2023-06-16 18:02:59 -0300

    Hello world

--- hello/hello.asm
+++ hello/hello.asm
@@ -1,10 +1,22 @@
 section .data
+    msg db "Hello, world", 0XA
+    tam equ $- msg
 
 section .text
 
 global _start
 
 _start:
+    mov EAX, 0X4
+    mov EBX, 0x1
+    mov ECX, msg
+    mov EDX, tam
+
+    int 0x80
+
+    output:
+
     ; destino, origem
     mov eax, 0x1
     mov ebx, 0x0
+    int 0x80
